6c28df32bbe1ffac85dccf2b6084a1f72719366d
Angel González <agonzalezbarragan@gmail.com>
2021-09-16 12:52:58 +0200

    ADDED: * formulario * db mock

--- src/app/views/login/login.component.ts
+++ src/app/views/login/login.component.ts
@@ -4,18 +4,20 @@ import { Router } from '@angular/router';
 
 @Component({
   selector: 'login',
-  templateUrl: './login.component.html'
+  templateUrl: './login.component.html',
 })
 export class LoginComponent {
-  public showFavorite: boolean = false
-  public groceries: any[] = [];
-  
+
   constructor(
     //private loginService: LoginService,
     private router: Router
   ) {}
 
   public navigateToTimer() {
-    this.router.navigate(['timer'])
+    this.router.navigate(['timer']);
+  }
+
+  public doLogin(email:string, password:string) {
+    alert(email)
   }
 }
